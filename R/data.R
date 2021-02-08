04bbd982dc96c31182ef4fee07b02465490e7201
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-02-08 12:01:34 -0500

    - Imported the data/ cytoband.rda files properly into the package and added documentation. - Removed outdate build_cytoband_rda R script since its not in DATASET.R

---
+++ R/data.R
@@ -0,0 +1,84 @@
+#' hg19 UCSC cytoband table
+#'
+#' A data table containing the cytoband and centromere information
+#' for hg19 genome
+#'
+#' @format A data frame with 862 rows and 5 variables:
+#' \describe{
+#'   \item{chrom}{Chromosome (e.g. chr1)}
+#'   \item{chromStart}{Chromosome cytoband start position (0-based)}
+#'   \item{chromEnd}{Chromosome cytoband end position (0-based)}
+#'   \item{name}{Name of cytoband}
+#'   \item{gieStain}{Giemsa staining}
+#'   ...
+#' }
+#' @source \url{https://genome.ucsc.edu/cgi-bin/}
+"ucsc.hg19.cytoband"
+
+#' hg38 UCSC cytoband table
+#'
+#' A data table containing the cytoband and centromere information
+#' for hg38 genome
+#'
+#' @format A data frame with 1433 rows and 5 variables:
+#' \describe{
+#'   \item{chrom}{Chromosome (e.g. chr1)}
+#'   \item{chromStart}{Chromosome cytoband start position (0-based)}
+#'   \item{chromEnd}{Chromosome cytoband end position (0-based)}
+#'   \item{name}{Name of cytoband}
+#'   \item{gieStain}{Giemsa staining}
+#'   ...
+#' }
+#' @source \url{https://genome.ucsc.edu/cgi-bin/}
+"ucsc.hg38.cytoband"
+
+#' mm10 UCSC cytoband table
+#'
+#' A data table containing the cytoband and centromere information
+#' for mm10 genome
+#'
+#' @format A data frame with 403 rows and 5 variables:
+#' \describe{
+#'   \item{chrom}{Chromosome (e.g. chr1)}
+#'   \item{chromStart}{Chromosome cytoband start position (0-based)}
+#'   \item{chromEnd}{Chromosome cytoband end position (0-based)}
+#'   \item{name}{Name of cytoband}
+#'   \item{gieStain}{Giemsa staining}
+#'   ...
+#' }
+#' @source \url{https://genome.ucsc.edu/cgi-bin/}
+"ucsc.mm10.cytoband"
+
+#' mm39 UCSC cytoband table
+#'
+#' A data table containing the cytoband and centromere information
+#' for mm39 genome
+#'
+#' @format A data frame with 121 rows and 5 variables:
+#' \describe{
+#'   \item{chrom}{Chromosome (e.g. chr1)}
+#'   \item{chromStart}{Chromosome cytoband start position (0-based)}
+#'   \item{chromEnd}{Chromosome cytoband end position (0-based)}
+#'   \item{name}{Name of cytoband}
+#'   \item{gieStain}{Giemsa staining}
+#'   ...
+#' }
+#' @source \url{https://genome.ucsc.edu/cgi-bin/}
+"ucsc.mm39.cytoband"
+
+#' mm9 UCSC cytoband table
+#'
+#' A data table containing the cytoband and centromere information
+#' for mm9 genome
+#'
+#' @format A data frame with 403 rows and 5 variables:
+#' \describe{
+#'   \item{chrom}{Chromosome (e.g. chr1)}
+#'   \item{chromStart}{Chromosome cytoband start position (0-based)}
+#'   \item{chromEnd}{Chromosome cytoband end position (0-based)}
+#'   \item{name}{Name of cytoband}
+#'   \item{gieStain}{Giemsa staining}
+#'   ...
+#' }
+#' @source \url{https://genome.ucsc.edu/cgi-bin/}
+"ucsc.mm9.cytoband"

74a443c442178466b49f3b4e0665eb4fe74d5452
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-02-09 12:55:05 -0500

    Added stats package to imports

--- build/as_build.R
+++ build/as_build.R
@@ -19,6 +19,8 @@ usethis::use_package("IRanges")
 usethis::use_package("S4Vectors")
 usethis::use_package("GenomeInfoDb")
 usethis::use_package("methods")
+usethis::use_package("stats")
+
 
 devtools::document(pkg)
 devtools::check(pkg)
@@ -26,7 +28,7 @@ devtools::build_vignettes(pkg)
 
 devtools::build(pkg)
 devtools::install(pkg)
-#devtools::install_github("quevedor2/aneuploidy_score")
+# devtools::install_github("quevedor2/aneuploidy_score")
 
 # library(AneuploidyScore)
 # AneuploidyScore::listData()

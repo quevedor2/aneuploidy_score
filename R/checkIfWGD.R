1758926b8818b6e6d50b7eee274291dfe2b8823d
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-02-10 17:29:59 -0500

    checkIfWGD() now just calls the .classifyCN() function

--- R/checkIfWGD.R
+++ R/checkIfWGD.R
@@ -30,18 +30,10 @@ checkIfWGD <- function(segf, tcn_col,threshold=0.5,
   seqlevelsStyle(seg_gr) <- 'UCSC'
   
   # Validation checks
-  loss_threshold <- wgd_ploidy - threshold
-  gain_threshold <- wgd_ploidy + threshold
-  if(loss_threshold < 0) stop("Loss threshold is below 0. Whole-genome duplication requires a total copy number (TCN) input, not L2R")
-  if(abs(loss_threshold-2) > 2) warning("A Total Copy Number (TCN) method is selected, but the loss threshold is too low to be informative")
-  if(abs(gain_threshold-2) > 2) warning("A Total Copy Number (TCN) method is selected, but the gain threshold is very distant from copy-neutral (2)")
-  
-  # Break down the CN genome into loss/neutral/gain
-  width_cn <- data.frame("width"=width(seg_gr), 
-                         "CN"=round(mcols(seg_gr)[,tcn_col],1))
-  cn_cuts <- cut(width_cn$CN, breaks = c(-0.1, loss_threshold, gain_threshold, 100))
-  levels(cn_cuts) <- c("LOSS", "NEUT", "GAIN")
-  width_cn$CN_stat <- as.character(cn_cuts)
+  cn_stat <- .classifyCN(cn=round(mcols(seg_gr)[,tcn_col],1), ploidy=2, threshold=threshold)
+  width_cn <- data.frame("width", width(seg_gr),
+                         "CN"=round(mcols(seg_gr)[,tcn_col],1),
+                         "CN_stat"=cn_stat)
   
   # Calculate the fraction of the genome for each loss, neut, gain
   total_genome_size <- sum(width_cn$width)

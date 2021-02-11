5a91b13cd66474e73127bc8cf47b53cdb9ebf744
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-02-11 11:01:01 -0500

    Updated the checkIfWGD() function to use multiple different methods to estimate the ploidy

--- R/checkIfWGD.R
+++ R/checkIfWGD.R
@@ -3,46 +3,61 @@
 #' @description Takes a .seg file data frame and estimates whether
 #' the tumor is whole-genome doubled (WGD). This function requries
 #' Total Copy Number and will not work using L2R.
+#'
 #' @param segf .Seg Data [Data frame]
 #' @param tcn_col Column ID for CN value [String] 
-#' @param threshold The threshold to call diploid
-#' (Diploid = wgd_ploidy +/- threshold) (Default: 0.5) [Numeric]
-#' @param wgd_gf Genomic fraction above wgd_ploidy
-#' to be considered genome-doubled (Default: 0.5) [Numeric]
-#' @param wgd_ploidy Base ploidy (Default: 2) [Integer]
-#'
+#' @param threshold The threshold to call a gain or loss relative
+#' to base ploidy (ploidy +/- threshold) (Default: 0.5) [Numeric]
+#' @param wgd_gf Genomic fraction above base_ploidy + threshold to be
+#' considered genome doubled (Default: 0.5) [Numeric]
+#' @param ploidy_method Calculates the base ploidy [Character]
+#'  - 'wmean': Segment size weighted CN mean
+#'  - 'wmedian': Segment size weighted CN median
+#'  - 'mean': CN mean
+#'  - 'median': CN median
+#'  - 'multi_base2': Nearest base ploidy 2 (e.g. CN=3 -> ploidy of 4)
+#' 
 #' @import GenomicRanges
 #' @importFrom GenomeInfoDb seqlevelsStyle
 #' @importFrom S4Vectors mcols
-#' @importFrom stats weighted.mean
+#' @importFrom matrixStats weightedMedian
+#' @importFrom matrixStats weightedMean
 #' @return A named vector
-#'  ploidy: The mean CN weighted by segment sizes
+#'  ploidy: Ploidy as estimated by ploidy_method 
 #'  WGD: Boolean indicated whether sample is WGD
 #'   
 #' @export
 checkIfWGD <- function(segf, tcn_col,threshold=0.5,
-                       wgd_gf = 0.5, wgd_ploidy=2){
+                       wgd_gf = 0.5, ploidy_method="wmean"){
   # tcn_col = 'Modal_Total_CN'
   stopifnot(.validateSeg(segf))
   
   ## Set up GenomicRange Objects of cytoband and seg files
-  seg_gr <- makeGRangesFromDataFrame(segf, keep.extra.columns = TRUE)
-  seqlevelsStyle(seg_gr) <- 'UCSC'
+  segf_gr <- makeGRangesFromDataFrame(segf, keep.extra.columns = TRUE)
+  seqlevelsStyle(segf_gr) <- 'UCSC'
+  segf_gr$CN <- mcols(segf_gr)[,tcn_col]
   
-  # Validation checks
-  cn_stat <- .classifyCN(cn=round(mcols(seg_gr)[,tcn_col],1), ploidy=2, threshold=threshold)
-  width_cn <- data.frame("width", width(seg_gr),
-                         "CN"=round(mcols(seg_gr)[,tcn_col],1),
-                         "CN_stat"=cn_stat)
+  ## Assign base ploidy to closest to a multi of base2 (e.g. 2,4,6,8)
+  wmean_ploidy <- weightedMean(segf_gr$CN, (width(segf_gr)/1*10^6))
+  ploidy_multi <- cut(wmean_ploidy, breaks=seq(-1,11,by=2), right=FALSE)
+  levels(ploidy_multi) <- seq(0,10, by=2)
   
+  ploidy_val <- switch(ploidy_method,
+                       "wmean"=wmean_ploidy,
+                       "wmedian"=weightedMedian(segf_gr$CN, (width(segf_gr)/1*10^6)),
+                       "mean"=mean(segf_gr$CN),
+                       "median"=median(segf_gr$CN),
+                       "multi_base2"=as.integer(as.character(ploidy_multi)),
+                       NA)
+
   # Calculate the fraction of the genome for each loss, neut, gain
-  total_genome_size <- sum(width_cn$width)
-  cn_genome_width <- sapply(split(width_cn, f=width_cn$CN_stat), function(i) sum(i$width))
-  cn_genome_fraction <- cn_genome_width / total_genome_size
+  total_genome_size <- sum(width(segf_gr))
+  cn_stat <- .classifyCN(cn=round(segf_gr$CN,1), ploidy=ploidy_val, threshold=threshold)
+  cn_genome_width <- sapply(split(width(segf_gr), f=cn_stat), sum)
+  cn_genome_fraction <- round(cn_genome_width / total_genome_size,3)
   
   # Check if WGD is present
-  WGD_status <- as.logical(cn_genome_fraction['GAIN'] > wgd_gf)
-  ploidy <- weighted.mean(width_cn$CN, width_cn$width/1000000)
-  
-  return(c("ploidy"=ploidy, "WGD"=WGD_status))
+  WGD_status <- as.logical(cn_genome_fraction['1'] > wgd_gf)
+
+  return(c("ploidy"=ploidy_val, "WGD"=WGD_status))
 }
